e1e719f67e13ee26f1c46a7b749e1cca0ce5cf02
Son94JB <125803158+Son94JB@users.noreply.github.com>
2024-08-14 15:43:13 +0900

    [Gold V] Title: AC, Time: 156 ms, Memory: 42604 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,35 +1,38 @@
+# R 뒤집기
+# D 첫 수 버리기
+# 빈 배열에서 D를 수행하면 error
 from collections import deque
- 
+
 t = int(input())
- 
-for i in range(t):
-    p = input()
-    n = int(input())
-    arr = input()[1:-1].split(',')
- 
-    queue = deque(arr)
- 
-    flag = 0
- 
-    if n == 0:
-        queue = []
- 
-    for j in p:
-        if j == 'R':
-            flag += 1
-        elif j == 'D':
-            if len(queue) == 0:
+
+for _ in range(t):
+
+    functions = input()
+    num = int(input())
+    data = input()[1:-1].split(',')
+    data = deque(data)
+
+    r_flag = 0
+
+    if num == 0:
+        data = []
+
+    for function in functions:
+        if function == "R":
+            r_flag += 1
+
+        elif function == "D":
+            if data:
+                if r_flag % 2 == 0:
+                    data.popleft()
+                else:
+                    data.pop()
+            else:
                 print("error")
                 break
-            else:
-                if flag % 2 == 0:
-                    queue.popleft()
-                else:
-                    queue.pop()
- 
     else:
-        if flag % 2 == 0:
-            print("[" + ",".join(queue) + "]")
+        if r_flag % 2 == 0:
+            print("[" + ",".join(data) + "]")
         else:
-            queue.reverse()
-            print("[" + ",".join(queue) + "]")
+            data.reverse()
+            print("[" + ",".join(data) + "]")
