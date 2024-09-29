9f38e48e065fa4d6a30603c9b1087105d6856c53
Son94JB <125803158+Son94JB@users.noreply.github.com>
2024-09-29 16:57:55 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,20 +1,20 @@
-N = int(input())
-num = 1
-if N == 1:
-    print("1/1")
-else:
-    for i in range(2*N):
-        if num + i <= N:
-            num += i
-            continue
-        else:
-            diff = N - num
-            if i % 2:
-                a = i - diff
-                b = 1 + diff
-            else:
-                a = 1 + diff
-                b = i - diff
-        print(f"{a}/{b}")
-        break
+def find_fraction(number):
+    n = 1
+    while number > n:
+        number -= n
+        n += 1
 
+    if n % 2:
+        numerator = n - number + 1
+        denominator = number
+
+    else:
+        numerator = number
+        denominator = n - number + 1
+        
+    return f"{numerator}/{denominator}"
+
+
+N = int(input())
+
+print(find_fraction(N))
